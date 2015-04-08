6e08ff8027184735ab3acd2539dd6b0ef0698d29
Moïse Valvassori <moise.valvassori@gmail.com>
2015-04-08 18:22:56 +0200

    formatting

--- server/init.js
+++ server/init.js
@@ -1,39 +1,46 @@
 Meteor.startup(function(){
-    if (! modules.collections.Presentations.findOne({name:'mypresentation'})) {
-	modules.collections.Presentations.insert({
-            name: 'mypresentation',
-            steps: [
-		{id:"s1",
-		 x:-1000,
-		 y:-1500,
-		 md: "My first presnetation",
-		 style:"clean",
-		 worldStyle:"clear"
-		},
-		{id:"s2",
-		 x:0,
-		 y:-1500,
-		 "rotate":"270", "scale":"3", "rotateY":"45",
-		 md: "A second slide",
-		 "interaction" : {
-                     "type" : "quiz",
-		     "data" : {
-			 "question" : "Question:",
-			 "responses" : [ 
-                             {
-				 "k" : "r1",
-				 "l" : "reponse 1"
-                             }, 
-                             {
-				 "k" : "r2",
-				 "l" : "reponse 2"
-                             }
-			 ]
-                     }}
-		}
-            ]
-	});
-    }
+  if (! modules.collections.Presentations.findOne({name:'mypresentation'})) {
+    modules.collections.Presentations.insert({
+      name: 'mypresentation',
+      steps: [
+	{id:"s1",
+	  x:-1000,
+	  y:-1500,
+	  md: "rrezy: reactive prezentation",
+	  style:"clean",
+	  worldStyle:"clear"
+	},
+	{id:"s2",
+	  x:0,
+	  y:-1500,
+	  "rotate":"270", "scale":"3", "rotateY":"45",
+	  md: "Don't you think that presentations given **in modern browsers** shouldn't **copy the limits** of 'classic' slide decks?",
+	  "interaction" : {
+            "type" : "quiz",
+	    "data" : {
+	      "question" : "Question:",
+	      "responses" : [ 
+                {
+		  "k" : "r1",
+		  "l" : "reponse 1"
+                }, 
+                {
+		  "k" : "r2",
+		  "l" : "reponse 2"
+                }
+	      ]
+            }}
+	},
+	{id:"s3",
+	  x:1000,
+	  y:-1500,
+	  style:"clean",
+	  worldStyle:"blue",
+	  md: "Would you like to **impress your audience** with **stunning visualization** of your talk?"
+	}
+      ]
+    });
+  }
 });
 
 
